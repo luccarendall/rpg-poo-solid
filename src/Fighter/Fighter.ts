2fcd758a59d83043b4c786220166b9f97dcfdc4c
Lucca Rendall <luccarendall2@gmail.com>
2022-10-06 13:54:52 -0300

    Classe Monster

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,16 +1,17 @@
 import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
 
-export default interface Fighter {
-  lifePoints: number;
-  strength: number;
+export default interface Fighter extends SimpleFighter {
+  // lifePoints: number;
+  // strength: number;
   defense: number;
   energy?: Energy;
 
-  attack(enemy: Fighter): void
+  // attack(enemy: Fighter): void
 
   special(enemy: Fighter): void
 
-  receiveDamage(amount: number): void
+  // receiveDamage(amount: number): void
 
   levelUp () : void
 
